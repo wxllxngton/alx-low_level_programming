136701cac18e992e5fdf9159ffd59984b160e2df
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-03-17 01:18:10 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+
 int main(void)
 {
 	char myStr[] = "\"Programming is like building a multilingual puzzle";
