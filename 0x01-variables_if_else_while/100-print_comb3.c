6f4a02655cc93c4393ca99f60173f2c53d59237e
John <jjngugi@gmail.com>
2023-03-17 01:50:59 -0700

    Files compiled

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,11 @@ int main(void)
     int u;
 
     for (s = 48; s < 58; s++) {
-        // Greater by one to make sure there are no reccurences such as 01 and 10
+        /* Greater by one to make sure there are no reccurences such as 01 and 10 */
         for (u = s + 1; u < 58; u++) {
             putchar(s);
             putchar(u);
-            // Only adds the separators if the number isn't the last number
+            /* Only adds the separators if the number isn't the last number */
             if (s != 56 || u != 57) {
                 putchar(',');
                 putchar(' ');
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,23 +13,17 @@ int main(void)
 	int s;
 	int u;
 
-    for (s = 0; s < 10; s++)
-        putchar(48 + s);
-        
-    // 65-90 Upper case letters
-    for (s = 65; s < 90; s++)
-        if (s >= 'G') 
-        {
-            break;
-        }
-        else 
-        {
-            // Converts to lowercase
-            u = tolower(s);
-            putchar(u);   
-        }
-
+	for (s = 48; s <= 57; s++)
+	{
+		putchar(s);
+	}
+	for (s = 65; s <= 70; s++)
+	{
+		u = tolower(s);
+		putchar(u);
+	}
 	putchar('\n');
 
 	return (0);
 }
+
