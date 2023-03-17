9def72f91d78ef998994cfa79fbde5b20a918ada
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-03-17 10:33:42 +0300

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int s;
+    int u;
+
+    for (s = 48; s < 58; s++) {
+        // Greater by one to make sure there are no reccurences such as 01 and 10
+        for (u = s + 1; u < 58; u++) {
+            putchar(s);
+            putchar(u);
+            // Only adds the separators if the number isn't the last number
+            if (s != 56 || u != 57) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
+}
