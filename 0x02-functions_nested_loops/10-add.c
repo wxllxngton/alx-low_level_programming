beedc25f36def1feeb8e48a5afae0cc1e4def5cd
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-03-21 12:07:30 +0300

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Check Description
+ * @x: integer
+ * @y: integer
+ * Description: function adds two integers together and goves result
+ * Return: Result after adding x and y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
