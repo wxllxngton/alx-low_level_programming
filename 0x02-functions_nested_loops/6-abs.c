20e109e4194459113dc0e083df66bc89a8e713dd
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-03-21 10:25:31 +0300

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Check Description
+ * @i: integer
+ * Description: Gives the absolute value of a given integer
+ *
+ * Return: absolute value of number i
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (i * -1);
+}
