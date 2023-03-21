907183f72da9d9a594a7c63eeae32a3efbe5c520
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-03-21 11:02:33 +0300

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Check Description
+ *
+ * Description: functions print minutes from 0000hrs to 2359hrs
+ *
+ * Return: Nothing
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
