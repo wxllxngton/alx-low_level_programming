1f93fd0a85963310743f196bbcc8d743521901c4
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-03-23 08:25:31 +0300

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - function prints 1 to 9
+ *
+ * Return: Nothing
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
