a7e364b57fd2639a7a9b7879b904ce2324b0d75b
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-03-24 00:00:55 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -13,13 +13,13 @@
 int _isdigit(int c)
 {
 	int isNum = 0;
-    for (int i = 48; i < 58; i++) 
-    {
-        if (i == c)
-        {
-            isNum = 1;
-            break;
-        }
-    }
-    return (isNum);
+	for (int i = 48; i < 58; i++) 
+	{
+		if (i == c)
+		{
+			isNum = 1;
+            		break;
+        	}
+    	}
+    	return (isNum);
 }
