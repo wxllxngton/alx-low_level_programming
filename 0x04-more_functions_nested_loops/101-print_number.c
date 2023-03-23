39220d0393c4d9f321f8a19c894b7fa1ec99d368
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-03-23 10:03:19 +0300

    Add files via upload

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - function prints a value
+ *
+ * @n: input
+ *
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -x;
+	}
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+	_putchar(x % 10 + '0');
+}
