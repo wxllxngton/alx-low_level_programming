176618b33e0d879e93cd083c7d3bfe153c2fb242
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-03-23 08:53:01 +0300

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14 ten times
+ *
+ * Return: Nothing
+ */
+
+void more_numbers(void)
+{
+	int n, c;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+				_putchar(c / 10 + '0');
+
+			_putchar(c % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
