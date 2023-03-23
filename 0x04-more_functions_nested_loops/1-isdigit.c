029b97ad9883dd8362d8ce7fa826ebe369efefbd
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-03-23 08:19:53 +0300

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check Description
+ *
+ * @c: input takes
+ * Description: functions checks if input is a digit
+ * or not
+ *
+ * Return: 1 if digit and 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+	int isNum = 0;
+    for (int i = 48; i < 58; i++) 
+    {
+        if (i == c)
+        {
+            isNum = 1;
+            break;
+        }
+    }
+    return (isNum);
+}
