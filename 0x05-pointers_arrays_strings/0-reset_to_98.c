afe9656ee7b4b968e13b11fe868e785a396bc17c
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-03-27 09:32:24 +0300

    Create 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function takes pointer and updates value to 98
+ *
+ * @n: input pointer
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	int x = 98;
+	*n = x;
+}
