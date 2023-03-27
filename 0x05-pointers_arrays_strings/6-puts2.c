c59d8f424e8dfee165c8b8831489688a15b5273c
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-03-27 10:48:44 +0300

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - function prints every character of a string
+ *
+ * @str: input string
+ *
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+    int counter, n = 0;
+    for (counter = 0; str[counter] != '\0'; ++counter);
+    
+    while (n < counter) 
+    {
+        _putchar(str[n]);
+        n++;
+    }
+    _putchar('\n');
+    
+}
