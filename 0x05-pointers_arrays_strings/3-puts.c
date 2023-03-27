86996924e826321c46430fea78ed666d35504bac
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-03-27 10:03:40 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - function prints a string, followed by newline
+ * to stdout
+ *
+ * @str: input string
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int n = 0;
+  int counter = 0;
+  while (str[counter] != '\0') 
+  {
+    // Since letters no use of + '0'
+    _putchar(str[counter]);
+    counter++;
+  }
+	_putchar('\n');
+}
