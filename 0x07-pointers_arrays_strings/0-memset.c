b2bff71cf3dd58767796c8e4204473b5710a9567
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-04-03 09:07:36 +0300

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - function files memory with constant byte
+ *
+ * @s: input string
+ * @b: input character
+ * @n: number of bytes
+ *
+ * Return: strings with constant byte
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int counter = 0;
+  while (counter < n) 
+  {
+      s[counter] = b; 
+      counter++;
+  }
+
+  return (s);
+}
