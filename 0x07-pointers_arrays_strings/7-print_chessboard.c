88f5d76dafc9064b0551a37691b8f6efe4bab1fb
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-04-05 03:03:02 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_chessboard: prints a chessboard
@@ -16,8 +17,8 @@ void print_chessboard(char (*a)[8])
 	{
 		for (col = 0; col < 8; col++)
 		{
-		    _putchar(a[row][col]);
+		    putchar(a[row][col]);
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
