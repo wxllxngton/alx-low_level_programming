e5d00ee3ac15d407978d835fb58ce248e455894a
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-04-10 20:39:50 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - function prints no. of arguments passed
+ * main - function prints the number of arguments passed into it.
  *
- * @argc: number of arguments
+ * @argc: length of arguments array
  * @argv: array of arguments
  *
  * Return: 0
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	if (argv[0])
+	if (argc > 0)
 		printf("%d\n", argc - 1);
 
 	return (0);
