9489a3fef3c16b42b48f6a0952dc8edaca953631
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-04-10 20:43:58 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,15 +3,16 @@
 
 /**
  * main - program multiplies 2 numbers
+ *
  * @argc: argument counter
- * @argv: argument vector containing values
+ * @argv: argument vector 
  *
  * Return: 0
  */
 
 int main(int argc, char **argv)
 {
-	int n1, n2, prod;
+	int x, y;
 
 	if (argc != 3)
 	{
@@ -20,10 +21,9 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		prod = n1 * n2;
-		printf("%d\n", prod);
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		printf("%d\n", x * y);
 		return (0);
 	}
 }
