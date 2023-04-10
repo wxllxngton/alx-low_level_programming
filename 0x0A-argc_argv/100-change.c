083690e9181c1e8cbbee5585a919e1ab5ea9bbd9
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-04-10 21:25:27 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,55 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- * main - program prints min no. of coins to make change
- * 
+ * main - program prints minimum number of coins to make change
+ *
  * @argc: argument counter
  * @argv: argument vector
- * 
+ *
  * Return: 1 if number of args pass, 0 otherwise
  */
- 
+
 int main(int argc, char **argv)
 {
-    int amount, num_coins = 0;
+    int amount, total_coins = 0;
+
     if (argc == 2)
     {
-    	amount = atoi(argv[1]);
-    	if (amount < 0)
-    	{
-    		printf("%d\n", 0);
-    		return (0);
-    	}
-    	if (amount % 25 >= 0)
-    	{
-    		num_coins += amount / 25;
-    		amount = amount % 25;
-    	}
-    	if (amount % 10 >= 0)
-    	{
-    		num_coins += amount / 10;
-    		amount = amount % 10;
-    	}
-    	if (amount % 5 >= 0)
-    	{
-    		num_coins += amount / 5;
-    		amount = amount % 5;
-    	}
-    	if (amount % 2 >= 0)
-    	{
-    		num_coins += amount / 2;
-    		amount = amount % 2;
-    	}
-    	if (amount % 1 >= 0)
-    		num_coins += amount;
-    	printf("%d\n", num_coins);
-    	return (0);
+        amount = atoi(argv[1]);
+
+        if (amount < 0)
+        {
+            printf("%d\n", 0);
+            return 0;
+        }
+
+        /* Calculate the number of quarters needed */
+        if (amount >= 25)
+        {
+            total_coins += amount / 25;
+            amount %= 25;
+        }
+
+        /* Calculate the number of dimes needed */
+        if (amount >= 10)
+        {
+            total_coins += amount / 10;
+            amount %= 10;
+        }
+
+        /* Calculate the number of nickels needed */
+        if (amount >= 5)
+        {
+            total_coins += amount / 5;
+            amount %= 5;
+        }
+
+        /* Calculate the number of pennies needed */
+        if (amount >= 1)
+        {
+            total_coins += amount / 1;
+            amount %= 1;
+        }
+
+        printf("%d\n", total_coins);
+        return 0;
     }
     else
     {
-    	printf("Error\n");
-    	return (1);
+        printf("Error\n");
+        return 1;
     }
 }
