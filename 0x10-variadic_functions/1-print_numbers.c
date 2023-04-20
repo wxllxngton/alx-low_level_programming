2133a09aca734fc564e2db1438c90e49cb1604c8
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-04-20 22:11:52 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,25 +9,25 @@
  * @n: number of parameters
  * @...: other parameters
  *
- * Return: all numbers to be printed
+ * Return: nothing
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list argsp;
+	va_list ap;
 	unsigned int i = 0;
-	int vals;
+	int argV;
 
-	va_start(argsp, n);
+	va_start(ap, n);
 	for (; i < n; i++)
 	{
-		vals = va_arg(argsp, int);
-		printf("%d", vals);
+		argV = va_arg(ap, int);
+		printf("%d", argV);
 		if (separator == NULL)
 			continue;
 		if (i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
-	va_end(argsp);
+	va_end(ap);
 }
