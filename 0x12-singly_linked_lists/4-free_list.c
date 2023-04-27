1f007bab4d3c4330aab49e238d8331cf3335728f
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-04-27 21:10:57 +0300

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list.
+ *
+ * @head: pointtr to the head
+ *
+ * Return: Nothing
+ */
+
+void free_list(list_t *head) 
+{
+    list_t *temp;
+    
+    while(head)
+    {
+        temp = head;
+        head = head->next;
+        free(temp->str);
+        free(temp);
+    }
+}
+
