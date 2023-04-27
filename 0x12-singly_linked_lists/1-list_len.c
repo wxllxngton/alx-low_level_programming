8366ab72869b7eafd0b0aba379547999a189e5e8
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-04-27 14:48:33 +0300

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ *
+ * @h: pointer to head of linked list
+ *
+ * Return: number of present nodes in the list
+ */
+
+size_t print_list(const list_t *h) 
+{
+    unsigned int nodesNo = 0;
+
+    while (h)
+    {
+      h = h->next;
+      nodesNo++;
+    }
+    return (nodesNo);
+}
