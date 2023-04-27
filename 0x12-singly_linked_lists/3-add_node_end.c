5168b3adae56c111546c1368a96c184604a18743
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-04-27 20:37:04 +0300

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,7 +12,8 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new = malloc(sizeof(list_t));
+    list_t *new;
+    new = malloc(sizeof(list_t));
     
     if (new == NULL) {
         return NULL;
@@ -22,11 +23,15 @@ list_t *add_node_end(list_t **head, const char *str)
     new->len = strlen(str);
     new->next = NULL;
     
-    if (*head == NULL) {
+    if (*head == NULL) 
+    {
         *head = new;
-    } else {
+    } 
+    else 
+    {
         list_t *current = *head;
-        while (current->next != NULL) {
+        while (current->next != NULL) 
+        {
             current = current->next;
         }
         current->next = new;
