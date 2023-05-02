08b64df3bab26ac7a4266c303ca25167170661de
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-05-02 10:09:53 +0300

    Create 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that returns the number of elements in a linked listint_t list.
+ * 
+ * @h: pointer to the first node of the list
+ * 
+ * Return: the number of nodes in the list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+    size_t len = 0;
+    
+    while (h)
+    {
+        h = h->next;
+        len++;
+    }
+    
+    return (len);
+}
