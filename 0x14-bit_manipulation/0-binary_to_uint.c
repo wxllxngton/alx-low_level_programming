0fa68f4e36990c6e08ddf384e551cb09d4638808
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-05-04 15:11:40 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,22 +8,22 @@
  * Return: the converted number, or 0 if error
  */
 
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char * b) 
 {
-    unsigned int decimal = 0;
-    int len, power = 0;
+  unsigned int decimal = 0;
+  int len, power = 0;
 
-    if (b == NULL)
-        return (0);
+  if (b == NULL)
+    return (0);
 
-    for (len = strlen(b) - 1; len >= 0; len--)
-    {
-        if (b[len] != '0' && b[len] != '1')
-            return (0);
+  for (len = strlen(b) - 1; len >= 0; len--) 
+  {
+    if (b[len] != '0' && b[len] != '1')
+      return (0);
 
-        decimal += (b[len] - '0') * pow(2, power);
-        power++;
-    }
+    decimal += (b[len] - '0') * pow(2, power);
+    power++;
+  }
 
-    return (decimal);
+  return (decimal);
 }
