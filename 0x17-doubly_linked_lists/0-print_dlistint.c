0d6ba19068a8ab973c6085aeafb54b8c1b5aaf93
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-15 16:08:59 +0300

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,14 +8,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t node_count = 0;
-
-    while (h)
-    {
-        printf("%d\n", h->n);
-        node_count++;
-        h = h->next;
-    }
-
-    return node_count;
+	size_t node_count = 0;
+	
+	while (h)
+	{
+		printf("%d\n", h->n);
+		node_count++;
+		h = h->next;
+	}
+	
+	return (node_count);
 }
