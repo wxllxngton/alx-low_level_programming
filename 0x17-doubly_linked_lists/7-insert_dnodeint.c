b9ae456c3a246595607066426596b6338ef6730c
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-15 21:56:34 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp = *h;
 
 	new_node = malloc(sizeof(dlistint_t));
-
 	if (new_node == NULL)
 		return (NULL);
 
@@ -50,8 +49,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (temp->next != NULL)
 		temp->next->prev = new_node;
-
 	temp->next = new_node;
-
 	return (new_node);
 }
