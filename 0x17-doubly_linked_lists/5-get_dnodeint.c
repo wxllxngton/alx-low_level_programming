3e2df705e8fe1c5c63ea26fcf941132611fe019c
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-15 21:44:51 +0300

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		if (counter == index)
 		{
-			return head;
+			return (head);
 		}
 		head = head->next;
 		counter++;
