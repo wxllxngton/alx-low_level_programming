bd3db359fa8a92fbdafa12dd661a9f16f3f0c1ab
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-15 16:16:26 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,47 +11,47 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    unsigned int counter = 0;
-    dlistint_t *new_node;
-    dlistint_t *temp = *h;
+	unsigned int counter = 0;
+	dlistint_t *new_node;
+	dlistint_t *temp = *h;
 
-    new_node = malloc(sizeof(dlistint_t));
+	new_node = malloc(sizeof(dlistint_t));
 
-    if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = NULL;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 
-    if (idx == 0)
-    {
-        new_node->next = *h;
-        if (*h != NULL)
-            (*h)->prev = new_node;
-        *h = new_node;
-        return (new_node);
-    }
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 
-    while (temp != NULL && counter < idx - 1)
-    {
-        temp = temp->next;
-        counter++;
-    }
+	while (temp != NULL && counter < idx - 1)
+	{
+		temp = temp->next;
+		counter++;
+	}
 
-    if (temp == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	if (temp == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->next = temp->next;
-    new_node->prev = temp;
+	new_node->next = temp->next;
+	new_node->prev = temp;
 
-    if (temp->next != NULL)
-        temp->next->prev = new_node;
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
 
-    temp->next = new_node;
+	temp->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
