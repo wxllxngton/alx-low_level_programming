2cc5deeb773012864840d6ff345660704c6a18e6
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-15 21:42:39 +0300

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,11 +7,11 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
-	
+
 	while (head != NULL)
 	{
-		temp = head->next; 
-		free(head); 
-		head = temp; 
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
