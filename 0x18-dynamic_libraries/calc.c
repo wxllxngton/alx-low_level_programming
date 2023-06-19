356738235ae0d7b988d9cec8d5739a75b65ee7fa
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-19 08:43:26 +0300

    Create calc.c

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,61 @@
+#include "calc.h"
+
+/**
+ * add - Adds two numbers and returns their result.
+ * @a: First number.
+ * @b: Second number.
+ * 
+ * Return: The sum of @a and @b.
+ */
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * sub - Subtracts two numbers and returns their result.
+ * @a: First number.
+ * @b: Second number.
+ * 
+ * Return: The difference between @a and @b.
+ */
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * mul - Multiplies two numbers and returns their result.
+ * @a: First number.
+ * @b: Second number.
+ * 
+ * Return: The product of @a and @b.
+ */
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * div - Divides two numbers and returns their result.
+ * @a: First number.
+ * @b: Second number.
+ * 
+ * Return: The quotient of @a divided by @b.
+ */
+int div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * mod - Calculates the modulo of two numbers and returns the result.
+ * @a: First number.
+ * @b: Second number.
+ * 
+ * Return: The modulo of @a divided by @b.
+ */
+int mod(int a, int b)
+{
+    return (a % b);
+}
