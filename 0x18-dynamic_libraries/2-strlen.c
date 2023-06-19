fd9b632b19d6f93f422a19a4be9ffd7d1f22a0d1
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-19 08:11:13 +0300

    Add files via upload

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function returns length of a string
+ *
+ * @s: input string
+ *
+ * Return: len of a string
+ */
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	/* for(n = 0; s[n] != '\0'; ++n); */
+	while (s[n] != '\0')
+		n++;
+
+	return (n);
+}
