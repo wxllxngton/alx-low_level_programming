1e44cd428624a3157e954ff7802fba1688b1e407
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-19 08:45:17 +0300

    Create operations.h

---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,49 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+/**
+ * add - Adds two numbers and returns their result.
+ * @a: First number.
+ * @b: Second number.
+ * 
+ * Return: The sum of @a and @b.
+ */
+int add(int a, int b);
+
+/**
+ * sub - Subtracts two numbers and returns their result.
+ * @a: First number.
+ * @b: Second number.
+ * 
+ * Return: The difference between @a and @b.
+ */
+int sub(int a, int b);
+
+/**
+ * mul - Multiplies two numbers and returns their result.
+ * @a: First number.
+ * @b: Second number.
+ * 
+ * Return: The product of @a and @b.
+ */
+int mul(int a, int b);
+
+/**
+ * div - Divides two numbers and returns their result.
+ * @a: First number.
+ * @b: Second number.
+ * 
+ * Return: The quotient of @a divided by @b.
+ */
+int div(int a, int b);
+
+/**
+ * mod - Calculates the modulo of two numbers and returns the result.
+ * @a: First number.
+ * @b: Second number.
+ * 
+ * Return: The modulo of @a divided by @b.
+ */
+int mod(int a, int b);
+
+#endif 
