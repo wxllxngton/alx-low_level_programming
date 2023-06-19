5718e323916ec7dceb37cb5cc41cb46a7b83be42
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-19 13:08:04 +0300

    Update 0-putchar.c

--- 0x18-dynamic_libraries/0-putchar.c
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -1,19 +1,13 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0 (Success)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int main(void)
+int _putchar(char c)
 {
-	char name[8] = "_putchar";
-	int n;
-
-	for (n = 0; n < 8; n++)
-		_putchar(name[n]);
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
