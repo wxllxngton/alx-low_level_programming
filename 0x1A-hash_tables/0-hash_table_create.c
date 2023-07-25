e4f1fd85989837ea27031a4f3248cc02a1cc6ed4
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-07-25 13:42:58 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,25 +9,25 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table;
-    unsigned long int i;
+	hash_table_t *table;
+	unsigned long int i;
 
-    table = malloc(sizeof(hash_table_t));
-    if (!table)
-        return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
 
-    table->size = size;
-    table->array = malloc(sizeof(hash_node_t *) * size);
-    if (!table->array)
-    {
-        free(table);
-        return (NULL);
-    }
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        table->array[i] = NULL;
-    }
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
 
-    return (table);
+	return (table);
 }
