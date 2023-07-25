e41fb70700dbe386651fb099884ff220c0f32e59
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-07-25 13:46:29 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,35 +10,35 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *new_node, *current_node;
-    unsigned long int index;
+	hash_node_t *new_node, *current_node;
+	unsigned long int index;
 
-    if (!ht || !key || strlen(key) == 0 || !value)
-        return (0);
+	if (!ht || !key || strlen(key) == 0 || !value)
+		return (0);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    current_node = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	current_node = ht->array[index];
 
-    while (current_node)
-    {
-        if (strcmp(current_node->key, key) == 0)
-        {
-            free(current_node->value); // Free existing value
-            current_node->value = strdup(value); // Duplicate and update value
-            return (1); // Update successful
-        }
-        current_node = current_node->next;
-    }
+	while (current_node)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value); /* Free existing value */
+			current_node->value = strdup(value); /* Duplicate and update value */
+			return (1); /* Update successful */
+		}
+		current_node = current_node->next;
+	}
 
-    // Key not found, create a new node and add it to the beginning of the list
-    new_node = malloc(sizeof(hash_node_t));
-    if (!new_node)
-        return (0);
+	/* Key not found, create a new node and add it to the beginning of the list */
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
 
-    new_node->key = strdup(key);
-    new_node->value = strdup(value);
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
-    return (1); // Successfully added new key-value pair
+	return (1); /* Successfully added new key-value pair */
 }
