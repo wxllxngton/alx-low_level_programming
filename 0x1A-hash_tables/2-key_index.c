1b0755f0ec842c75e022960e5badeccd338bb4f6
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-07-25 13:44:11 +0300

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,10 +10,10 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash_value, index;
+	unsigned long int hash_value, index;
 
-    hash_value = hash_djb2(key);
-    index = hash_value % size;
+	hash_value = hash_djb2(key);
+	index = hash_value % size;
 
-    return (index);
+	return (index);
 }
