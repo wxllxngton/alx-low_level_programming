345a45f60be727a6c83f64591b1b26dfbae5c31a
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-20 17:33:32 +0300

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,17 +14,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t index;
+	size_t index;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    for (index = 0; index < size; index++)
-    {
-        printf("Value checked array[%lu] = %d\n", index, array[index]);
-        if (array[index] == value)
-            return (index);
-    }
+	for (index = 0; index < size; index++)
+	{
+		printf("Value checked array[%lu] = %d\n", index, array[index]);
+		if (array[index] == value)
+			return (index);
+	}
 
-    return (-1);
+	return (-1);
 }
