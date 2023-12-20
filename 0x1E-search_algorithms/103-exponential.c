87eca5e833a70320f622a27d02054b80b2e4e0c8
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-20 17:15:34 +0300

    Update 103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -7,27 +7,29 @@
  * @array: Pointer to an array.
  * @size: Length of the array.
  * @value: Value to search for.
- * 
+ *
  * Return:
  *  - Index of the searched value.
  *  - If value is not present in array or if array is NULL, returns -1.
  */
 int exponential_search(int *array, size_t size, int value) 
 {
-	size_t index = 1;
+	size_t index;
 	int result;
-
-	while (index < size && array[index] <= value) 
+	
+	index = 1
+	
+	while (index < size && array[index] <= value)
 	{
 		printf("Value checked array[%d] = [%d]\n", (int)index, array[index]);
 		index *= 2;
 	}
-
+	
 	printf("Value found between indexes [%d] and [%d]\n", (int)(index / 2), (int)(index - 1));
-
-	result = binary_search(array + (index / 2), 
+	
+	result = binary_search(array + (index / 2),
 	                        ((index / 2) > size) ? size : (index / 2), 
 	                        value);
-
+	                        
 	return (result == -1 ? result : result + (int)(index / 2));
 }
