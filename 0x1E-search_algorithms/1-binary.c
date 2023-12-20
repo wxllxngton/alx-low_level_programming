83a36b662c3966de5b271974808d7ca66cc44d49
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-20 14:46:19 +0300

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,29 +12,29 @@
  *  - (Index of the searched value)
  *  - (If value is not present in array or if array is NULL, returns -1)
  */
-int binary_search(int *array, size_t size, int value) 
+int binary_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    size_t left = 0;
-    size_t right = size - 1;
+	size_t left = 0;
+	size_t right = size - 1;
 
-    while (left <= right) 
-    {
-        size_t middle = (left + right) / 2;
+	while (left <= right) 
+	{
+		size_t middle = (left + right) / 2;
 
-        printf("Searching in array: ");
-        for (size_t i = left; i <= right; i++)
-            printf("%d%s", array[i], (i < right) ? ", " : "\n");
+		printf("Searching in array: ");
+		for (size_t i = left; i <= right; i++)
+			printf("%d%s", array[i], (i < right) ? ", " : "\n");
 
-        if (array[middle] == value)
-            return (middle);
-        else if (array[middle] < value)
-            left = middle + 1;
-        else
-            right = middle - 1;
-    }
+		if (array[middle] == value)
+			return (middle);
+		else if (array[middle] < value)
+			left = middle + 1;
+		else
+			right = middle - 1;
+	}
 
-    return (-1); // Value not found
+	return (-1);
 }
