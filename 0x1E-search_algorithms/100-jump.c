b0c527af4ccf274a77dc72b9438e09bf8975153b
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-20 16:31:24 +0300

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,14 +3,15 @@
 /**
  * jump_search - Searches for a value in a sorted array of integers using
  * the Jump search algorithm.
- * 
+ *
  * @array: Pointer to an array.
  * @size: Length of the array.
  * @value: Value to search for.
- * 
+ *
  * Return:
  *  - (First index where the value is located)
- *  - (If the value is not present in the array or if the array is NULL, returns -1)
+ *  - (If the value is not present in the array or 
+ *  - if the array is NULL, returns -1)
  */
 int jump_search(int *array, size_t size, int value) 
 {
@@ -19,11 +20,11 @@ int jump_search(int *array, size_t size, int value)
 
 	size_t jump = (size_t)sqrt(size);
 	size_t index = 0;
-
+	
 	while (index < size && array[index] < value) 
 	{
 		printf("Value checked array[%lu] = [%d]\n", index, array[index]);
-
+		
 		if (index + jump < size) 
 		{
 			if (array[index + jump] >= value) 
@@ -31,19 +32,19 @@ int jump_search(int *array, size_t size, int value)
 				break;
 			}
 			index += jump;
-		} 
-		else 
+		}
+		else
 		{
 			break;
 		}
 	}
 
 	printf("Value found between indexes [%lu] and [%lu]\n", index, index + jump);
-
+	
 	for (size_t i = index; i <= index + jump && i < size; i++) 
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value) 
+		if (array[i] == value)
 		{
 			return ((int)i);
 		}
