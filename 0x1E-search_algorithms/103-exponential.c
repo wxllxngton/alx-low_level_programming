62a53ff27365e03d7a9c67931d1ddb05a0cfa0d6
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-20 17:52:05 +0300

    Update 103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * exponential_search - Searches for a value in a sorted array 
+ * exponential_search - Searches for a value in a sorted array
  * of integers using the Exponential search algorithm.
  *
  * @array: Pointer to an array.
@@ -25,12 +25,12 @@ int exponential_search(int *array, size_t size, int value)
 		index *= 2;
 	}
 
-	printf("Value found between indexes [%d] \ 
+	printf("Value found between indexes [%d] \
 	and [%d]\n", (int)(index / 2), (int)(index - 1));
 	
 	result = binary_search(array + (index / 2),
-	                        ((index / 2) > size) ? size : (index / 2), 
-	                        value);
-	                        
+	                ((index / 2) > size) ? size : (index / 2),
+	                    value);
+	                    
 	return (result == -1 ? result : result + (int)(index / 2));
 }
